9649bdd656c37e2f88f2cf92ce20747eec39df86
Tony <tony@kideduc.com>
2021-09-07 14:41:30 +0800

    clone form Nora, first init version
    first version , clone from Nora

---
+++ src/js/routes.js
@@ -0,0 +1,323 @@
+
+import HomePage from '../pages/home.f7.html';
+import AboutPage from '../pages/about.f7.html';
+import FormPage from '../pages/form.f7.html';
+import CatalogPage from '../pages/catalog.f7.html';
+import SchedulePage from '../pages/schedule.f7.html';
+import ProductPage from '../pages/product.f7.html';
+import SettingsPage from '../pages/settings.f7.html';
+
+import DynamicRoutePage from '../pages/dynamic-route.f7.html';
+import RequestAndLoad from '../pages/request-and-load.f7.html';
+import NotFoundPage from '../pages/404.f7.html';
+
+import LeftPage1 from '../pages/left-page-1.f7.html';
+import LeftPage2 from '../pages/left-page-2.f7.html';
+
+import CruiseAjaxLoad from '../pages/cruise-ajax-load.f7.html';
+import CruiseAjaxLinkSumry from '../pages/cruise-ajax-link-summry.f7.html';
+
+import AboutLinkAjaxLoadPage from '../pages/about-link-ajax.f7.html';
+
+var routes = [
+  {
+    path: '/',
+    component: HomePage,
+  },
+  {
+    path: '/about/',
+    component: AboutPage,
+  },
+  {
+    path: '/about-link-ajax/:linkId',
+    async: function ({ router, to, resolve }) {
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var linkId = to.params.linkId;
+
+      // Simulate Ajax Request
+      setTimeout(function () {
+        // We got user data from request
+        var vCatg = {
+          firstName: 'Dear',
+          lastName: 'My Passengers',
+          about: ' 我们为您提供以下服务，希望您喜欢!',
+          links: [
+            {
+              title: 'Fall in lover with YangY Website',
+              url: 'http://framework7.io',
+            },
+            {
+              title: 'Loving Eva Forum',
+              url: 'http://forum.framework7.io',
+            },
+          ]
+        };
+        // Tony Adding Main Logic of AJAX :  1. Larabel 2. CCS Studio
+        app.request.get('https://www.kideduc.com/portal/services/view_link.php?link_id='+linkId, function(data) {
+              // Hide Preloader
+              app.preloader.hide();
+
+              // Resolve route to load page
+              resolve(
+                  {
+                      component: AboutLinkAjaxLoadPage,
+                  },
+                  {
+                      props: {
+                          catg: vCatg,
+                          prdt: data,
+                      }
+                  }
+              );
+
+          });
+/*
+        // Hide Preloader
+        app.preloader.hide();
+
+        // Resolve route to load page
+        resolve(
+          {
+            component: AboutLinkAjaxLoadPage,
+          },
+          {
+            props: {
+              catg: catg,
+            }
+          }
+        );
+*/
+      }, 1000);
+    },
+  },
+  {
+    path: '/form/',
+    component: FormPage,
+  },
+  {
+    path: '/left-page-1/',
+    component: LeftPage1,
+  },
+  {
+    path: '/left-page-2/',
+    component: LeftPage2,
+  },
+  {
+    path: '/catalog/',
+    component: CatalogPage,
+  },
+  {
+    path: '/schedule/',
+    component: SchedulePage,
+  },
+  {
+    path: '/product/:id/',
+    component: ProductPage,
+  },
+  {
+    path: '/settings/',
+    component: SettingsPage,
+  },
+
+  {
+    path: '/dynamic-route/blog/:blogId/post/:postId/',
+    component: DynamicRoutePage,
+  },
+  {
+    path: '/request-and-load/user/:userId/',
+    async: function ({ router, to, resolve }) {
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var userId = to.params.userId;
+
+      // Simulate Ajax Request
+      setTimeout(function () {
+        // We got user data from request
+        var user = {
+          firstName: 'Vladimir',
+          lastName: 'Kharlampidi',
+          about: 'Hello, i am creator of Framework7! Hope you like it!',
+          links: [
+            {
+              title: 'Framework7 Website',
+              url: 'http://framework7.io',
+            },
+            {
+              title: 'Framework7 Forum',
+              url: 'http://forum.framework7.io',
+            },
+          ]
+        };
+        // Hide Preloader
+        app.preloader.hide();
+
+        // Resolve route to load page
+        resolve(
+          {
+            component: RequestAndLoad,
+          },
+          {
+            props: {
+              user: user,
+            }
+          }
+        );
+      }, 1000);
+    },
+  },
+  {
+    path: '/request-and-load/catg/:catgId/',
+    async: function ({ router, to, resolve }) {
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var cateId = to.params.catgId;
+
+      // Simulate Ajax Request
+      setTimeout(function () {
+        // We got user data from request
+        var vCatg = {
+          firstName: 'Dear',
+          lastName: 'My Passengers',
+          about: '我们为您提供以下特色服务，希望您喜欢!',
+          links: [
+            {
+              title: 'Fall in lover with YangY Website',
+              url: 'http://framework7.io',
+            },
+            {
+              title: 'Loving Eva Forum',
+              url: 'http://forum.framework7.io',
+            },
+          ]
+        };
+        // Tony Adding Main Logic of AJAX :  1. Larabel 2. CCS Studio
+//      app.request.get('https://localhost:8083/products', function(data) {
+        app.request.get('https://www.kideduc.com/portal/services/link_categories.php?node='+cateId, function(data) {
+              // Hide Preloader
+              app.preloader.hide();
+
+              // Resolve route to load page
+              resolve(
+                  {
+                      component: CruiseAjaxLoad,
+                  },
+                  {
+                      props: {
+                          catg: vCatg,
+                          prdt: data,
+                      }
+                  }
+              );
+
+          });
+/*
+        // Hide Preloader
+        app.preloader.hide();
+
+        // Resolve route to load page
+        resolve(
+          {
+            component: CruiseAjaxLoad,
+          },
+          {
+            props: {
+              catg: catg,
+            }
+          }
+        );
+*/
+      }, 1000);
+    },
+  },
+  {
+    path: '/request-and-load/links/:catgId/',
+    async: function ({ router, to, resolve }) {
+      // App instance
+      var app = router.app;
+
+      // Show Preloader
+      app.preloader.show();
+
+      // User ID from request
+      var cateId = to.params.catgId;
+
+      // Simulate Ajax Request
+      setTimeout(function () {
+        // We got user data from request
+        var vCatg = {
+          firstName: 'Dear',
+          lastName: 'My Passengers',
+          about: ' 我们为您提供以下服务，希望您喜欢!',
+          links: [
+            {
+              title: 'Fall in lover with YangY Website',
+              url: 'http://framework7.io',
+            },
+            {
+              title: 'Loving Eva Forum',
+              url: 'http://forum.framework7.io',
+            },
+          ]
+        };
+        // Tony Adding Main Logic of AJAX :  1. Larabel 2. CCS Studio
+//      app.request.get('https://localhost:8083/products', function(data) {
+        app.request.get('https://www.kideduc.com/portal/services/links.php?node='+cateId, function(data) {
+              // Hide Preloader
+              app.preloader.hide();
+
+              // Resolve route to load page
+              resolve(
+                  {
+                      component: CruiseAjaxLinkSumry,
+                  },
+                  {
+                      props: {
+                          catg: vCatg,
+                          prdt: data,
+                      }
+                  }
+              );
+
+          });
+/*
+        // Hide Preloader
+        app.preloader.hide();
+
+        // Resolve route to load page
+        resolve(
+          {
+            component: CruiseAjaxLinkSumry,
+          },
+          {
+            props: {
+              catg: catg,
+            }
+          }
+        );
+*/
+      }, 1000);
+    },
+  },
+  {
+    path: '(.*)',
+    component: NotFoundPage,
+  },
+];
+
+export default routes;
---
+++ src/js/store.js
@@ -0,0 +1,170 @@
+
+import { createStore } from 'framework7';
+
+const store = createStore({
+  state: {
+    loading: false,
+    users: [],
+    events: [],
+    artidaily: [],
+    myarrange: [],
+    mysuggest: [],
+
+
+    synday: '2021/01/01',
+    access: [],
+
+    products: [
+      {
+        id: '1',
+        title: 'Apple iPhone 8',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
+      },
+      {
+        id: '2',
+        title: 'Apple iPhone 8 Plus',
+        description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
+      },
+      {
+        id: '3',
+        title: 'Apple iPhone X',
+        description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
+      },
+    ]
+  },
+  getters: {
+    loading({ state }) {
+      return state.loading;
+    },
+    users({ state }) {
+      return state.users;
+    },
+    events({ state }) {
+      return state.events;
+    },
+    artidaily({ state }) {
+      return state.artidaily;
+    },
+    myarrange({ state }) {
+      return state.myarrange;
+    },
+    mysuggest({ state }) {
+      return state.mysuggest;
+    },
+
+    synday({ state }) {
+      return state.synday;
+    },
+    access({ state }) {
+      return state.access;
+    },
+    products({ state }) {
+      return state.products;
+    }
+  },
+  actions: {
+    getUsers({ state }) {
+      state.loading = true;
+      setTimeout(() => {
+//      state.users = ['User 1', 'User 2', 'User 3', 'User 4', 'User 5'];
+        state.users = [{ "id" : "1", "login" : "AAA", "first_name" : "GuestAAA", "last_name" : "Member", "title" : "", "group" : "1", "mobile" : "", "address1" : "", "address2" : "", "address3" : "", "date_last_login" : "11/24/2000", "language_id" : "4", "image_url" : "", "age_id" : "3", "gender_id" : "1", "education_id" : "2", "user_is_active" : "1" }, { "id" : "2", "login" : "BBB", "first_name" : "AdminBBB", "last_name" : "User", "title" : "", "group" : "2", "mobile" : "", "address1" : "", "address2" : "", "address3" : "", "date_last_login" : "12/26/2000", "language_id" : "4", "image_url" : "", "age_id" : "4", "gender_id" : "1", "education_id" : "3", "user_is_active" : "1" }, { "id" : "3", "login" : "CCC", "first_name" : "YangCCC", "last_name" : "Yang", "title" : "", "group" : "2", "mobile" : "", "address1" : "中关村18号", "address2" : "金地格林13栋4-203", "address3" : "东城区五道口", "date_last_login" : "", "language_id" : "", "image_url" : "", "age_id" : "", "gender_id" : "", "education_id" : "", "user_is_active" : "1" }, { "id" : "4", "login" : "DDD", "first_name" : "EvaDDD", "last_name" : "Bai", "title" : "", "group" : "1", "mobile" : "", "address1" : "中关村18号", "address2" : "金地格林13栋4-203", "address3" : "东城区五道口", "date_last_login" : "", "language_id" : "", "image_url" : "", "age_id" : "", "gender_id" : "", "education_id" : "", "user_is_active" : "1" }];
+        state.loading = false;
+      }, 3000);
+    },
+    getUsersBy({ state }, { login , password }) {
+      state.loading = true;
+      setTimeout(() => {
+          fetch(`https://www.kideduc.com/portal/services/auth.php?login=${login}&password=${password}`)
+            .then((res) => res.json())
+            .then((users) => {
+              state.loading = false;
+              state.users = users['users'];
+            })
+      }, 3000);
+    },
+    getEvents({ state }, { catgID }) {
+      state.loading = true;
+      setTimeout(() => {
+          fetch(`https://www.kideduc.com/portal/services/events.php?catgID=${catgID}`)
+            .then((res) => res.json())
+            .then((events) => {
+              state.loading = false;
+              state.events = events['events'];
+            })
+      }, 3000);
+    },
+    getArtiDaily({ state }, { userID }) {
+      state.loading = true;
+      setTimeout(() => {
+          fetch(`https://www.kideduc.com/portal/services/articles_daily.php?userID=${userID}`)
+            .then((res) => res.json())
+            .then((artidaily) => {
+              state.loading = false;
+              state.artidaily = artidaily['dailydetails'];
+            })
+      }, 3000);
+    },
+    getMyArrange({ state }, { userID, f7 }) {
+      state.loading = true;
+      setTimeout(() => {
+          fetch(`https://www.kideduc.com/portal/services/articles_arrg.php?userID=${userID}`)
+            .then((res) => res.json())
+            .then((myarrange) => {
+              state.loading = false;
+              f7.preloader.hide();  // hidden refresh icon...tony add
+              state.myarrange = myarrange['arranges'];
+            })
+      }, 3000);
+    },
+    getMySuggest({ state }, { userID, f7 }) {
+      state.loading = true;
+      setTimeout(() => {
+          fetch(`https://www.kideduc.com/portal/services/articles_sugg.php?userID=${userID}`)
+            .then((res) => res.json())
+            .then((mysuggest) => {
+              state.loading = false;
+              f7.preloader.hide();  // hidden refresh icon...tony add
+              state.mysuggest = mysuggest['suggestions'];
+            })
+      }, 3000);
+    },
+
+    addAccess({ state }, access) {
+      state.access = [...state.access, access];
+    },
+    addProduct({ state }, product) {
+      state.products = [...state.products, product];
+    },
+    resetProduct({ state }) {
+      state.products = [
+                          {
+                            id: '1',
+                            title: 'Apple iPhone 8',
+                            description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi tempora similique reiciendis, error nesciunt vero, blanditiis pariatur dolor, minima sed sapiente rerum, dolorem corrupti hic modi praesentium unde saepe perspiciatis.'
+                          },
+                          {
+                            id: '2',
+                            title: 'Apple iPhone 8 Plus',
+                            description: 'Velit odit autem modi saepe ratione totam minus, aperiam, labore quia provident temporibus quasi est ut aliquid blanditiis beatae suscipit odio vel! Nostrum porro sunt sint eveniet maiores, dolorem itaque!'
+                          },
+                          {
+                            id: '3',
+                            title: 'Apple iPhone X',
+                            description: 'Expedita sequi perferendis quod illum pariatur aliquam, alias laboriosam! Vero blanditiis placeat, mollitia necessitatibus reprehenderit. Labore dolores amet quos, accusamus earum asperiores officiis assumenda optio architecto quia neque, quae eum.'
+                          },
+                        ];
+    },
+    resetMyArrange({ state }, { userID }) { // Dom should refresh, otherwise deleted-itme will be always exist ! 
+      state.loading = true;
+      setTimeout(() => {
+          fetch(`https://www.kideduc.com/portal/services/articles_arrg.php?userID=${userID}`)
+            .then((res) => res.json())
+            .then((myarrange) => {
+              state.loading = false;
+              state.myarrange = myarrange['arranges'];
+            })
+      }, 1000);
+    },
+  },
+})
+export default store;
